96951191b9082a1831d6a61d2a84c47fe32210b1
anthonymnf <anthonymnf30@gmail.com>
2024-04-01 16:10:28 -0300

    Primeiras rotas

---
+++ src/pages/cart.tsx
@@ -0,0 +1,18 @@
+import { NextPage } from "next";
+import Head from "next/head";
+
+const Cart: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Carrinho</title>
+        <meta name="description" content="Meu carrinho de compras" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <h1>Carrinho</h1>
+    </>
+  );
+};
+
+export default Cart;
---
+++ src/pages/products.tsx
@@ -0,0 +1,18 @@
+import { NextPage } from "next";
+import Head from "next/head";
+
+const Products: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Nossos Produtos</title>
+        <meta name="description" content="Conheça todos os nossos produtos" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <h1>Nossos Produtos</h1>
+    </>
+  );
+};
+
+export default Products;
